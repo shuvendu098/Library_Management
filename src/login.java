0aed1a4b6df8172cd784459d2df53e024d7cf8e4
parshuram786 <107826907+parshuram786@users.noreply.github.com>
2022-06-28 23:09:21 +0530

    Update login.java

--- src/login.java
+++ src/login.java
@@ -1,7 +1,16 @@
+<<<<<<< Updated upstream
 public class login
 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         
     }
+=======
+public class login
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+    }
+>>>>>>> Stashed changes
 }
